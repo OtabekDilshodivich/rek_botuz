bd081971205cba50364df2a2563a035d4535bdea
rustamov.otabek.dilshodivich <159309496+OtabekDilshodivich@users.noreply.github.com>
2025-04-10 22:01:27 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "7685576928:AAGN-pVA6tJCGpoSerbE9C8iVKSjYhVqk7I"
 ADMIN_ID = 5685141670
 WEBHOOK_PATH = "/webhook"
-WEBHOOK_URL = "https://your-app-name.onrender.com/webhook"
+WEBHOOK_URL = "https://rek-botuz.onrender.com/webhook"
